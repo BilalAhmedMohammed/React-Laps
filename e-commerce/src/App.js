de14630e83c8a411761ed5ddaedea764897ed991
BilalAhmedMohammed <bilalelmaestro2019@gmail.com>
2022-03-03 14:50:14 +0200

    add login and register components

---
+++ e-commerce/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+// import Login from './Components/Login/Login';
+import Register from './Components/Register/Register';
+function App() {
+  return (
+    // <Login/>
+    <Register />
+  );
+}
+
+export default App;
