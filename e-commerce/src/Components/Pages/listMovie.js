2107b5ee3575a11b345ae4c6c82030c90a2cdb0c
BilalAhmedMohammed <bilalelmaestro2019@gmail.com>
2022-03-05 07:26:21 +0200

    add search to Movie

--- e-commerce/src/Components/Pages/listMovie.js
+++ e-commerce/src/Components/Pages/listMovie.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Movie from "../Movie/Movie";
+import "./list.css";
 import ReactPaginate from "react-paginate";
 import { axiosInstance } from "../Network/axiosConfig";
 import Pagination from "../Pagination/Pagination";
@@ -7,11 +8,13 @@ import Pagination from "../Pagination/Pagination";
 export default function ListMovie() {
   const [page, setPage] = useState([]);
   const [pageCount, setPageCount] = useState(100);
+  const [movies, setMovies] = useState([]);
+  const [searchTitle, setsearchTitle] = useState("");
+
   const handelPageClick = (data) => {
     let currentPage = data.selected + 1;
     setPage(currentPage);
   };
-  const [movies, setMovies] = useState([]);
   useEffect(() => {
     axiosInstance
       .get("/popular", {
@@ -27,15 +30,31 @@ export default function ListMovie() {
   }, [page]);
   return (
     <>
-      <div id="products" className="container-fluid">
+      <div id="movies" className="container-fluid">
+        <input
+          className="input-group-text w-75 mb-3 ms-5"
+          type={"search"}
+          placeholder={"Search By Movie Name"}
+          onChange={(ev)=>setsearchTitle(ev.target.value)}
+        />
         <div className="row">
-          {movies.map((movie) => {
-            return (
-              <div key={movie.id} className="col-4">
-                <Movie movie={movie} />
-              </div>
-            );
-          })}
+          {movies
+            .filter((value) => {
+              if (searchTitle === "") return value;
+              else if (
+                value.title
+                  .toLowerCase()
+                  .includes(searchTitle.toLocaleLowerCase())
+              )
+                return value;
+            })
+            .map((movie) => {
+              return (
+                <div key={movie.id} className="col-4">
+                  <Movie movie={movie} />
+                </div>
+              );
+            })}
         </div>
       </div>
       <div className="mt-5">
